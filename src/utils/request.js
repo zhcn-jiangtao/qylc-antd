7b1a238e47e82c2a14612d46b67d286456b7bb37
leon-girl <hustme@126.com>
2020-03-29 19:29:53 +0800

    更多

--- src/utils/request.js
+++ src/utils/request.js
@@ -38,6 +38,8 @@ const errorHandler = error => {
             message: `请求错误 ${status}: ${url}`,
             description: errorText,
         });
+        response.error = true;
+        response.msg = errorText;
     } else if (!response) {
         notification.error({
             description: '您的网络发生异常，无法连接服务器',
